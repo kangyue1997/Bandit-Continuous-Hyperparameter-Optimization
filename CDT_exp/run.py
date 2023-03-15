5317b199fedb4f5355de31d7e3ce632853ba16c0
Yue Kang <64324301+kangyue1997@users.noreply.github.com>
2023-03-15 15:38:12 -0700

    Update run.py

--- CDT_exp/run.py
+++ CDT_exp/run.py
@@ -7,7 +7,6 @@ from algo.GLM_TSL import *
 from algo.GLOC import *
 from algo.data_generator import *
 
-
 import os
 
 import pandas as pd
@@ -89,6 +88,7 @@ methods = {
     'theory': '_theoretical_explore',
     'auto': '_auto',
     'op': '_op',
+    'tl': '_tl',
 }
 
 # if not os.path.exists('results/'):
@@ -118,6 +118,7 @@ def func(n0):
     reg_theory = np.zeros(t)
     reg_auto = np.zeros(t)
     reg_op = np.zeros(t)
+    reg_tl = np.zeros(t)
 
     if algo == 'linucb':
         algo_class = LinUCB(data)
@@ -141,28 +142,34 @@ def func(n0):
         reg_theory += fcts['theory'](lamda = lamda, delta = delta)
         reg_op += fcts['op'](explore_rates = exp_rate, lamda = lamda)
         reg_auto += fcts['auto'](exp_time = 30, inte = inte, lamda=lamda)
+        reg_tl += fcts['tl'](explore_rates=exp_rate, lamda=lamda)
     elif algo == 'ucbglm':
         reg_theory += fcts['theory'](lamda = lamda, delta = delta)
         reg_op += fcts['op'](explore_rates = exp_rate, lamda = lamda)
         reg_auto += fcts['auto'](exp_time = 90, inte = inte, lamda=lamda)
+        reg_tl += fcts['tl'](explore_rates=exp_rate, lamda=lamda)
     elif algo == 'laplacets':
         reg_theory += fcts['theory'](lamda=lamda)
         reg_op += fcts['op'](explore_rates=exp_rate)
         reg_auto += fcts['auto'](exp_time=80, inte=inte)
+        reg_tl += fcts['tl'](explore_rates=exp_rate)
     elif algo == 'glmtsl':
         reg_theory += fcts['theory'](tau = 150,lamda=lamda)
         reg_op += fcts['op'](explore_rates=exp_rate,tau = 150,lamda=lamda)
         reg_auto += fcts['auto'](tau = 150, inte=inte, lamda = lamda)
+        reg_tl += fcts['tl'](explore_rates=exp_rate, tau=150, lamda=lamda)
     elif algo == 'gloc':
         reg_theory += fcts['theory']()
         reg_op += fcts['op'](explore_rates=exp_rate)
         reg_auto += fcts['auto'](inte=inte)
+        reg_tl += fcts['tl'](explore_rates=exp_rate, k = exp_rate)
     else:
         reg_theory += fcts['theory'](C = c, eta0 = eta0)
         reg_op += fcts['op'](C = c, explore_rates=exp_rate)
         reg_auto += fcts['auto'](C = c, inte=inte)
-    print("theory {0}, auto {1}, op {2}".format(reg_theory[-1], reg_auto[-1], reg_op[-1]))
-    return reg_theory, reg_auto, reg_op
+        reg_tl += fcts['tl'](explore_rates={'eta': exp_rate, 'alpha': exp_rate}, C=c)
+    print("theory {0}, auto {1}, op {2}, tl {3}".format(reg_theory[-1], reg_auto[-1], reg_op[-1], reg_tl[-1]))
+    return reg_theory, reg_auto, reg_op, reg_tl
 
 with concurrent.futures.ProcessPoolExecutor() as executor:
     secs = [nn for nn in range(rep)]
@@ -171,6 +178,7 @@ with concurrent.futures.ProcessPoolExecutor() as executor:
 res1 = [i[0] for i in list(results)]
 res2 = [i[1] for i in list(results)]
 res3 = [i[2] for i in list(results)]
+res4 = [i[2] for i in list(results)]
 
 if eff:
     final = np.array([a[-1] for a in res2])
@@ -178,6 +186,7 @@ if eff:
     res1 = [res1[order[i+2]] for i in range(pick)]
     res2 = [res2[order[i+2]] for i in range(pick)]
     res3 = [res3[order[i+2]] for i in range(pick)]
+    res4 = [res4[order[i+2]] for i in range(pick)]
     rep = pick
 
 
@@ -186,16 +195,19 @@ print('mean')
 print('{0}: reg_theory: {1}'.format(algo,(sum(res1)/rep)[-5:]))
 print('{0}: reg_auto: {1}'.format(algo,(sum(res2)/rep)[-5:]))
 print('{0}: reg_op: {1}'.format(algo,(sum(res3)/rep)[-5:]))
+print('{0}: reg_tl: {1}'.format(algo, (sum(res4) / rep)[-5:]))
 
 std_theory = [np.std([a[i] for a in res1]) for i in range(t)]
 std_auto = [np.std([a[i] for a in res2]) for i in range(t)]
 std_op = [np.std([a[i] for a in res3]) for i in range(t)]
+std_tl = [np.std([a[i] for a in res4]) for i in range(t)]
 
 print('\n')
 print('std')
 print('{0}: std_theory: {1}'.format(algo,list(std_theory[-5:])))
 print('{0}: std_auto: {1}'.format(algo,list(std_auto)[-5:]))
 print('{0}: std_op: {1}'.format(algo,list(std_op)[-5:]))
+print('{0}: std_tl: {1}'.format(algo, list(std_tl)[-5:]))
 print('\n')
 print('\n')
 print('\n')
@@ -214,7 +226,7 @@ if save:
         file += str(v) + '_'
     file = file[:-1] + '.csv'
     
-    df = pd.DataFrame({'theory' : sum(res1)/rep, 'auto': sum(res2)/rep, 'op' : sum(res3)/rep, 'std_theory': std_theory, 'std_auto': std_auto, 'std_op': std_op})
+    df = pd.DataFrame({'theory' : sum(res1)/rep, 'auto': sum(res2)/rep, 'op' : sum(res3)/rep, 'tl': sum(res4)/rep, 'std_theory': std_theory, 'std_auto': std_auto, 'std_op': std_op, 'std_tl':std_tl})
     df.to_csv(file)
 
 
