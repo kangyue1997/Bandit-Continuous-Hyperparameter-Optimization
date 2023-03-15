56abb49bfce0c2dd1e899b5e9376d93bd5f3b3dc
Yue Kang <64324301+kangyue1997@users.noreply.github.com>
2023-03-15 15:22:33 -0700

    Update GLM_TSL.py

--- CDT_exp/algo/GLM_TSL.py
+++ CDT_exp/algo/GLM_TSL.py
@@ -189,3 +189,59 @@ class GLM_TSL:
             s, f, index = op_tuning(s, f, observe_r, index)
             explore = explore_rates[index]
         return regret
+    
+    def glmtsl_tl(self, explore_rates, tau = 150, lamda = 1):
+        T = self.T
+        d = self.data.d
+        regret = np.zeros(T)
+
+        y = np.array([])
+        y = y.astype('int')
+        X = np.empty([0, d])
+        for t in range(tau):
+            feature = self.data.fv[t]
+            K = len(feature)
+            pull = np.random.choice(K)
+            observe_r = self.data.random_sample(t, pull)
+            y = np.concatenate((y, [observe_r]), axis=0)
+            X = np.concatenate((X, [feature[pull]]), axis=0)
+            regret[t] = regret[t - 1] + self.data.optimal[t] - self.data.reward[t][pull]
+
+        if y[0] == y[1]:
+            y[1] = 1 - y[0]
+
+        explore_lamda = explore_rates
+        Kexp = len(explore_lamda)
+        logw = np.zeros(Kexp)
+        p = np.ones(Kexp) / Kexp
+        gamma = min(1, math.sqrt(Kexp * math.log(Kexp) / ((np.exp(1) - 1) * T)))
+        # random initial explore rate
+        index = np.random.choice(Kexp)
+        explore = explore_lamda[index]
+
+        for t in range(tau, T):
+            feature = self.data.fv[t]
+            K = len(feature)
+            ucb_idx = [0] * K
+            clf = LogisticRegression(penalty='none', fit_intercept=False, solver='lbfgs').fit(X, y)
+            theta_bar = clf.coef_[0]
+            H = np.identity(d) * lamda
+            for l in range(t):
+                tmp = self.mu_dot(X[l].dot(theta_bar))
+                H += np.outer(X[l], X[l]) * tmp
+            H_inv = np.linalg.inv(H)
+            # for instable posterior solving due to unsuitable parameters, end early
+            if np.isnan(H_inv).any() or np.isinf(H_inv).any() or np.isnan(theta_bar).any() or np.isinf(theta_bar).any():
+                regret[-1] = float('Inf')
+                break
+            theta = np.random.multivariate_normal(theta_bar, explore ** 2 * H_inv)
+            for arm in range(K):
+                ucb_idx[arm] = feature[arm].dot(theta)
+            pull = np.argmax(ucb_idx)
+            observe_r = self.data.random_sample(t, pull)
+            y = np.concatenate((y, [observe_r]), axis=0)
+            X = np.concatenate((X, [feature[pull]]), axis=0)
+            regret[t] = regret[t - 1] + self.data.optimal[t] - self.data.reward[t][pull]
+            logw, p, index = tl_auto_tuning(logw, p, observe_r, index, gamma)
+            explore = explore_lamda[index]
+        return regret
