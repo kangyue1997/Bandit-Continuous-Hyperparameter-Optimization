378441e2c9e7f584141a4d5e2c589e15a139ca97
Yue Kang <64324301+kangyue1997@users.noreply.github.com>
2023-03-15 15:24:11 -0700

    Update GLOC.py

--- CDT_exp/algo/GLOC.py
+++ CDT_exp/algo/GLOC.py
@@ -141,4 +141,50 @@ class GLOC:
             explore = explore_rates[index]
         return regret
 
+    def gloc_tl(self, explore_rates, k, eta = 1, S = 1, lamda = 1, eps = 1):
+        T = self.T
+        d = self.data.d
+        regret = np.zeros(self.T)
+        A = eps * np.identity(d)
+        A_inv = 1/eps * np.identity(d)
+        V_inv = 1/lamda * np.identity(d)
+        theta_hat = np.zeros(d)
+        theta = np.zeros(d)
+        xz = np.zeros(d)
+
+        a = [explore_rates, k]
+        explore_rates = list(it.product(*(a[kv] for kv in range(len(a)))))
+        Kexp = len(explore_rates)
+        logw = np.zeros(Kexp)
+        p = np.ones(Kexp) / Kexp
+        gamma = min(1, math.sqrt(Kexp * math.log(Kexp) / ((np.exp(1) - 1) * T)))
+        # random initial hyper-para
+        index = np.random.choice(Kexp)
+        explore = explore_rates[index][0]
+        k = explore_rates[index][1]
+
+        for t in range(T):
+            feature = self.data.fv[t]
+            K = len(feature)
+            ucb_idx = [0]*K
+            for arm in range(K):
+                ucb_idx[arm] = feature[arm].dot(theta_hat) + explore * math.sqrt(feature[arm].dot(V_inv).dot(feature[arm]))
+            pull = np.argmax(ucb_idx)
+            observe_r = self.data.random_sample(t, pull)
+            gs = self.grad(theta, feature[pull], observe_r)
+            regret[t] = regret[t-1] + self.data.optimal[t] - self.data.reward[t][pull]
+            tmp = A_inv.dot(feature[pull])
+            A_inv -= np.outer(tmp, tmp)/ (1+feature[pull].dot(tmp))
+            A += np.outer(feature[pull], feature[pull])
+            tmp = V_inv.dot(feature[pull])
+            V_inv -= np.outer(tmp, tmp)/ (1+feature[pull].dot(tmp))
+            theta_prime = theta - gs / k * A_inv.dot(feature[pull])
+            theta = self.argm(theta_prime, A, S, eta)
+            xz += feature[pull].dot(theta) * feature[pull]
+            theta_hat = V_inv.dot(xz)
+
+            logw, p, index = tl_auto_tuning(logw, p, observe_r, index, gamma)
+            explore = explore_rates[index][0]
+            k = explore_rates[index][1]
+        return regret
 
