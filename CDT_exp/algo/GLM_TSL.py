c2e5410e7fa082d4ac793e519a1b522f34f6aa00
Yue Kang <64324301+kangyue1997@users.noreply.github.com>
2023-03-17 10:25:12 -0700

    Update GLM_TSL.py

--- CDT_exp/algo/GLM_TSL.py
+++ CDT_exp/algo/GLM_TSL.py
@@ -117,7 +117,7 @@ class GLM_TSL:
                 time = [1]
                 rad = [math.sqrt(c * math.log(T))]
                 sd = [math.sqrt(2 * c)]
-                explore = trans(4,cen[ind],1)
+                explore = trans(4,cen[0],1)
             else:
                 ind, cen, time, mu, rad, sd = auto_tuning(cen, time, rad, sd, c, T, mu, inte)
                 explore =  trans(4,cen[ind],1)
