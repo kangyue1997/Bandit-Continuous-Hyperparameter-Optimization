f3607c5428534a7b0fe098fb29ed4a7ff23db445
Yue Kang <64324301+kangyue1997@users.noreply.github.com>
2023-03-17 10:26:48 -0700

    Update LinUCB.py

--- CDT_exp/algo/LinUCB.py
+++ CDT_exp/algo/LinUCB.py
@@ -79,7 +79,7 @@ class LinUCB:
         sd = [math.sqrt(2*c)]
         feature = self.data.fv[exp_time]
         K = len(feature)
-        explore = trans(4,cen[ind],1)
+        explore = trans(4,cen[0],1)
         ucb_idx = [0] * K
 
         for arm in range(K):
@@ -107,7 +107,7 @@ class LinUCB:
                 time = [1]
                 rad = [math.sqrt(c * math.log(T))]
                 sd = [math.sqrt(2 * c)]
-                explore = trans(4,cen[ind],1)
+                explore = trans(4,cen[0],1)
             else: 
                 ind, cen, time, mu, rad, sd = auto_tuning(cen, time, rad, sd, c, T, mu, inte)
                 explore = trans(4,cen[ind],1)
